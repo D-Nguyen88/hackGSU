b42f0cc2b58ae6df25a8ac2dcc890203efd71a03
Christian Gerhardt <cgerhardt@hntb.com>
2018-10-06 14:21:32 -0400

    updated models

--- FoodTruckApp/models.py
+++ FoodTruckApp/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+import re
+from django.core.validators import RegexValidator, MinValueValidator, MaxValueValidator
 # Create your models here.
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
@@ -17,25 +19,26 @@ def create_profile(sender, **kwargs):
 post_save.connect(create_profile, sender=User)
 
 weekdays = [
-        (1, _("Monday")),
-        (2, _("Tuesday")),
-        (3, _("Wednesday")),
-        (4, _("Thursday")),
-        (5, _("Friday")),
-        (6, _("Saturday")),
-        (7, _("Sunday")),
+        (1, ("Monday")),
+        (2, ("Tuesday")),
+        (3, ("Wednesday")),
+        (4, ("Thursday")),
+        (5, ("Friday")),
+        (6, ("Saturday")),
+        (7, ("Sunday")),
     ]
 
 class Truck(models.Model):
-    truck_name = models.CharsField(max_length = 50)
-    truck_description = models.CharsField(max_length = 150)
-    truck_phone_number = models.CharsField(max_length = 12)
-    match_phone_number = re.search(r'^(\d{3}--\d{3}--\d{4})',truck_phone_number)
+    truck_name = models.CharField(max_length = 50)
+    truck_description = models.CharField(max_length = 150)
+    truck_phone_number_regex = RegexValidator(regex=r'^\d{3}--\d{3}--\d{4})', message= "Phone number must be entered in the format: '+999-999-9999'.")
+    truck_phone_number = models.CharField(validators=[truck_phone_number_regex], max_length = 15 )
+    # match_phone_number = re.search(r'^(\d{3}--\d{3}--\d{4})',truck_phone_number)
         #Formats phone number to xxx-xxx-xxxx
-    truck_email_address = models.CharsField(max_length = 50)
+    truck_email_address = models.CharField(max_length = 50)
     match_truck_email_address = (r'\w+@\w+', truck_email_address)
         #Formats email address field name@example.com
-    rating = DoubleField(default=0.0)
+    rating = models.FloatField(default=0.0)
         #Rating for new Truck is set to 0.0
 
     
@@ -51,29 +54,29 @@ class Announcements(models.Model):
 
 class TruckList(models.Model):
     user = models.ForeignKey(UserProfile)
-    truck = ForeignKey(Truck)
-    rating = FloatField(min=1.0, max=5.0, default=0.0)
+    truck = models.ForeignKey(Truck)
+    rating = models.FloatField(validators=[MinValueValidator(1.0), MaxValueValidator(5.0)], default=0.0)
 
-class Header(models.Model)
-    header_name = models.CharsField(max_length = 50)
+class Header(models.Model):
+    header_name = models.CharField(max_length = 50)
 
 class Menu(models.Model):
-    truck = ForeignKey(Truck)
-    header = ForeignKey(Header)
+    truck = models.ForeignKey(Truck)
+    header = models.ForeignKey(Header)
 
 class Item(models.Model):
-    header = ForeignKey(Header)
-    item_name = models.CharsField(max_length = 50)
-    item_description = model.CharsField(max_lenth = 100)
-    item_price = model.FloatField(null=True, blank=True, default=None)
+    header = models.ForeignKey(Header)
+    item_name = models.CharField(max_length = 50)
+    item_description = models.CharField(max_length = 100)
+    item_price = models.FloatField(null=True, blank=True, default=None)
 
 class Tags(models.Model):
-    tag_name = model.CharsField(max_length = 20)
+    tag_name = models.CharField(max_length = 20)
 
 #Links Truck and Tags models
 class Truck_Tag(models.Model):
-    tag = ForeignKey(Tags)
-    truck = ForeignKey(Truck)
+    tag = models.ForeignKey(Tags)
+    truck = models.ForeignKey(Truck)
 
 class Profile_Picture(models.Model):
     image = models.ImageField(upload_to='profile_image', default = 'profile_image/img_avatar2.png')
